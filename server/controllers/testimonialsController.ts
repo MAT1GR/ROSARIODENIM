8f394c562eb178a5063b7f89b83a447d068d5f1b
MAT1GR <grigomati@gmail.com>
2025-11-19 23:50:16 -0300

    naziiiultimo

--- server/controllers/testimonialsController.ts
+++ server/controllers/testimonialsController.ts
@@ -17,34 +17,6 @@ const testimonials = [
   },
   {
     id: 3,
-    customerName: 'Valentina G.',
-    content: 'La curaduría que tienen es espectacular. Siempre encuentro joyitas que no se ven en otro lado.',
-    rating: 5,
-    productName: 'Jean Mom Clásico'
-  },
-  {
-    id: 4,
-    customerName: 'Lucia P.',
-    productName: 'Jean Mom " небеса"',
-    rating: 5,
-    content: '¡Me encantó el calce! Súper cómodo y de excelente calidad. ¡Lo recomiendo!',
-  },
-  {
-    id: 5,
-    customerName: 'Martina G.',
-    productName: 'Jean Cargo "шторм"',
-    rating: 5,
-    content: 'El cargo es increíble, el color y la tela son geniales. Me llegó rapidísimo.',
-  },
-  {
-    id: 6,
-    customerName: 'Sofia R.',
-    productName: 'Jean Wide Leg "пустыня"',
-    rating: 5,
-    content: 'Hacía mucho que buscaba un wide leg de este color. ¡Es perfecto!.',
-  },
-  {
-    id: 7,
     customerName: 'Leimai V.',
     productName: 'Jean Skinny "eclipse"',
     rating: 5,
